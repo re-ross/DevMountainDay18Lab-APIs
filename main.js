0c1b2ad1bf8417227e49ef1788ad2fb0e8192094
Ryan Ross <ryanross@Bretts-MBP.attlocal.net>
2022-01-27 17:06:41 -0600

    added reset button with subideal functionality

--- main.js
+++ main.js
@@ -1,20 +1,21 @@
 const getResidents = document.querySelector(".get-residents");
-
-const itWorks = () => {
-  console.log("it works!");
-};
+const resetBtn = document.querySelector(".reset");
+const divElement = document.querySelector("div");
 
 getResidents.addEventListener(
   "click",
   axios.get("https://swapi.dev/api/planets/2").then((res) => {
     for (let i = 0; i < res.data.residents.length; i++) {
       axios.get(res.data.residents[i]).then((res) => {
-        const bodyElement = document.body;
         const h2 = document.createElement("h2");
         const text = document.createTextNode(`${res.data.name}`);
         h2.appendChild(text);
-        bodyElement.appendChild(h2);
+        divElement.appendChild(h2);
       });
     }
   })
 );
+
+resetBtn.addEventListener("click", () => {
+  divElement.removeChild(divElement.firstChild);
+});
