edacd923aa4ec4e9a6f8096c8546dbd5c5ad90cd
Ryan Ross <ryanross@Bretts-MBP.attlocal.net>
2022-01-27 20:26:09 -0600

    added improved functionality

--- main.js
+++ main.js
@@ -1,21 +1,48 @@
-const getResidents = document.querySelector(".get-residents");
-const resetBtn = document.querySelector(".reset");
-const divElement = document.querySelector("div");
+// const getResidents = document.querySelector(".get-residents");
+// const resetBtn = document.querySelector(".reset");
+// const divElement = document.querySelector("div");
+// const bodyElement = document.querySelector("body");
 
-getResidents.addEventListener(
-  "click",
+// const click = () => {
+//   axios.get("https://swapi.dev/api/planets/2").then((res) => {
+//     for (let i = 0; i < res.data.residents.length; i++) {
+//       axios.get(res.data.residents[i]).then((res) => {
+//         const h2 = document.createElement("h2");
+//         const text = document.createTextNode(`${res.data.name}`);
+//         h2.appendChild(text);
+//         divElement.appendChild(h2);
+//       });
+//     }
+//   });
+
+//   getResidents.addEventListener("click", click);
+
+//   resetBtn.addEventListener("click", () => {
+//     divElement.innerHTML = ``;
+//   });
+// };
+
+const resetBtn = document.querySelector(".reset");
+const btn = document.querySelector("button");
+const bodyEl = document.querySelector("div");
+
+function clear() {
+  bodyEl.innerHTML = "";
+}
+
+function click() {
   axios.get("https://swapi.dev/api/planets/2").then((res) => {
     for (let i = 0; i < res.data.residents.length; i++) {
       axios.get(res.data.residents[i]).then((res) => {
-        const h2 = document.createElement("h2");
+        const h2URL = document.createElement("h2");
         const text = document.createTextNode(`${res.data.name}`);
-        h2.appendChild(text);
-        divElement.appendChild(h2);
+        h2URL.appendChild(text);
+        bodyEl.appendChild(h2URL);
       });
     }
-  })
-);
+  });
+}
 
-resetBtn.addEventListener("click", () => {
-  divElement.removeChild(divElement.firstChild);
-});
+btn.addEventListener("click", click);
+
+resetBtn.addEventListener("click", clear);
